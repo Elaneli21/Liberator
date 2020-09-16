f593125744dbae191d17e242c1eb8b4f1e8dd017
Iradia <masamone123@gmail.com>
2020-09-16 18:18:12 +0700

    PlayerMovement script update
    player udah bisa jalan

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private Rigidbody2D playerRb;
+    private float moveHor;
+    private float moveVer;
+    private float moveLimit = 0.7f;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveHor = Input.GetAxisRaw("Horizontal");
+        moveVer = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        if (moveHor != 0 && moveVer != 0)
+        {
+            moveHor *= moveLimit;
+            moveVer *= moveLimit;
+        }
+
+        playerRb.velocity = new Vector2(moveHor * speed, moveVer * speed);
+    }
+
+}
