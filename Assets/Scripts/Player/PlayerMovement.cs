bacb56b7c015b50c31a810a6da9e75cd84e4612c
Iradia <masamone123@gmail.com>
2020-10-04 17:31:51 +0700

    Implement player_run & player_idle animation, Fixing some codes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,31 +6,46 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
 
-    private Rigidbody2D playerRb;
+    private Rigidbody2D rigid;
+    private Animator anim;
+    private SpriteRenderer spriteRen;
+
     private float moveHor;
     private float moveVer;
     private float moveLimit = 0.7f;
 
     void Start()
     {
-        playerRb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    void Update()
     {
+        //receive input for movement
         moveHor = Input.GetAxisRaw("Horizontal");
         moveVer = Input.GetAxisRaw("Vertical");
+
+        //flip sprite according to move direction
+        if (moveHor != 0) spriteRen.flipX = (moveHor < 0);
+
+        //play animation when doing action
+        if (moveHor != 0 || moveVer != 0) anim.Play("Player_Run");
+        else if (moveHor == 0 || moveVer == 0) anim.Play("Player_Idle");
     }
 
     void FixedUpdate()
     {
+        //limit sideway movement speed
         if (moveHor != 0 && moveVer != 0)
         {
             moveHor *= moveLimit;
             moveVer *= moveLimit;
         }
 
-        playerRb.velocity = new Vector2(moveHor * speed, moveVer * speed);
+        //implement movement
+        rigid.velocity = new Vector2(moveHor * speed, moveVer * speed);
     }
 
 }
